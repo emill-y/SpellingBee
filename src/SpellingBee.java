7133de882c80748133a0cd70a6d286c3f96b3ca4
emill-y <eishayadav@gmail.com>
2025-03-14 11:40:24 -0700

    Added Final Comments, and finished initial requirements.

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -42,44 +42,60 @@ public class SpellingBee {
     }
 
     public void generate() {
-        generate("", letters,new boolean[letters.length()]);
+        // Call Helper Generate Function that Accepts the Letters
+        // And also accepts if the letter has been utilized yet or not (to avoid repetition and infinite recursion)
+        generate("", letters, new boolean[letters.length()]);
     }
 
     public void generate(String substring, String leftover, boolean[] used){
+        // Adds the generated substring if it exists, and is not aldready present in the Array List
         if (!substring.isEmpty() && !words.contains(substring)) {
             words.add(substring);
         }
+        // Only will run if there are still characters leftover to form permutations with
+        // Hence preventing Stack Overflow
         for (int i = 0; i < leftover.length(); i++) {
-            // Skip already used characters
+            // Skip already used characters (To Avoid Infinite Recursion + Repetition)
             if (used[i]) continue;
             used[i] = true;
+            // Recursively With Unused Letter Added to Substring
             generate(substring + leftover.charAt(i), leftover, used);
+            // Set the character to avalible again, so it can be reutilized in the next word
             used[i] = false;
         }
     }
 
     public void sort() {
-        words = mergeSort(words, 0, words.size() - 1);
+        // Recursively Calls MergeSort Method
+        words = mergeSort(0, words.size() - 1);
     }
 
-    private ArrayList<String> mergeSort(ArrayList<String> arr, int low, int high) {
-
+    private ArrayList<String> mergeSort(int low, int high) {
+        // Base Case Checks if the indicies of high and low are Equal
+        // Meaning that only one element is left (and hence is inherently sorted)
         if (high <= low) {
             ArrayList<String> newArr = new ArrayList<>();
             newArr.add(words.get(low));
             return newArr;
         }
-
+        // Finds Middle of The Array
         int med = (high + low) / 2;
-        ArrayList<String> arr1 = mergeSort(arr, low, med);
-        ArrayList<String> arr2 = mergeSort(arr, med + 1, high);
+        // Sorts Each Half of the Array
+        ArrayList<String> arr1 = mergeSort(low, med);
+        ArrayList<String> arr2 = mergeSort(med + 1, high);
+        // Sorts the two individual arrays
         return merge(arr1, arr2);
 
     }
 
+    // Merge Method that Is Called from MergeSort
     public ArrayList<String> merge(ArrayList<String> arr1, ArrayList<String> arr2) {
+        // Merged ArrayList that will be returned
         ArrayList<String> merged = new ArrayList<>();
+
+        // Initialize base indicies that will iterate over each array at different rates
         int a = 0, b = 0;
+        // Three While Statements- 2 Will Be run
         while (a < arr1.size() && b < arr2.size()) {
             if (arr1.get(a).compareTo(arr2.get(b)) > 0) {
                 merged.add(arr1.get(a));
@@ -115,6 +131,11 @@ public class SpellingBee {
     public void checkWords() {
         // Remove word under the condition that it is not within the dictionary.
         // Method Learnt from Oracle Library for Java
+        // Follows the Format:
+        // Removes all elements satisfying the given predicate,
+        // from index i (inclusive) to index end (exclusive).
+        // Predicate: Not in the dictionary (i.e, Binary Search returned false, and didn't find it)
+        // word is an individual element in the Array List words
         words.removeIf(word -> !binarySearch(word));
     }
 
@@ -122,12 +143,22 @@ public class SpellingBee {
     public boolean binarySearch(String target){
         int left = 0;
         int right = DICTIONARY_SIZE - 1;
+
         while (left <= right){
             int mid = left + (right - left) / 2;
+            // Utilize CompareTo Value to see if strings are equal,
+            // if not, move in the correct direction of the dictionary (i.e to the left or right)
             int comparison = DICTIONARY[mid].compareTo(target);
-            if (comparison == 0) return true;
-            if (comparison < 0) left = mid + 1;
-            else right = mid - 1;
+            if (comparison == 0){
+                // Return true if element found in Dictionary
+                return true;
+            }
+            if (comparison < 0) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
         return false;
     }
